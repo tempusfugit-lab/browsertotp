cf3b3bc72ea80886bd4ade284478354c5ddf68d4
yiida-tempus <iida@tempusfugit.jp>
2016-05-09 10:50:53 +0900

    add some files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,31 @@
 module.exports = function (grunt) {
   'use strict';
 
+  // Configurable paths
+  var config = {
+    app: 'app',
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    copy: {
+      appjs: {
+        files: [
+          {expand: true, cwd: "bower_components/blirhotp/dist/", src: ["*.min.js"], dest: "app/scripts/"},
+          {expand: true, cwd: "bower_components/Sortable/", src: ["*.min.js"], dest: "app/scripts/"},
+        ]
+      }
+    },
+  });
+  grunt.registerTask('build', [
+    'copy:appjs',
+  ]);
+
+  grunt.registerTask('default', [
+    'build',
+  ]);
 };
---
+++ README.md
@@ -0,0 +1,28 @@
+# browsertotp
+
+TOTP running on your web browser.  
+The implementations **never** access internet and the outside,
+runnning on local only. So they can run standalone.
+
+Your key used for creating a TOTP value will be saved into `window.localStorage` (
+if not supported, will saved on memory).
+
+## Installation
+
+Download this repository.
+
+````
+git clone <repository url>
+````
+
+## Usage
+
+View `index.html` in a browser.
+
+## Brower Support
+
+Modern browsers running on Desktop or Mobile.
+
+## License
+
+See LICENSE file.
